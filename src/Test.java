63c2485b57b8d007d8ce7e995cacce898cbf6573
Syed-Fahad-Ali-27 <syed.fahad.ali2704@gmail.com>
2025-12-01 19:14:51 +0100

    The Game Begins

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Get Ready for an exciting adventure!!");
+        System.out.println("Welcome to the Crown Jewel Heist Game");
     }
 
 }
