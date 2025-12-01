faefd7235c52a3e0f36f57e74c8ed05f4a7b53a1
Syed-Fahad-Ali-27 <syed.fahad.ali2704@gmail.com>
2025-12-01 19:09:39 +0100

    Some print lines

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,8 @@
 public class Test {
 
     public static void main(String[] args) {
-
+        System.out.println("Hello World");
+        System.out.println("Get Ready for an exciting adventure!!");
     }
 
 }
